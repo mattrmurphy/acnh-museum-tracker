2bfe6be5c240fe99cad955473145ccea48421bf2
Matt <mmurphydev@gmail.com>
2020-07-23 23:48:57 -0400

    adds routes and respective component imports to router.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,70 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../views/Index.vue'
+import Fish from '../views/Fish.vue'
+import Bugs from '../views/Bugs.vue'
+import Fossils from '../views/Fossils.vue'
+import Art from '../views/Art.vue'
+import Museum from '../views/museum/Index.vue'
+import MuseumFish from '../views/museum/Fish.vue'
+import MuseumBugs from '../views/museum/Bugs.vue'
+import MuseumFossils from '../views/museum/Fossils.vue'
+import MuseumArt from '../views/museum/Art.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: Index
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/fish',
+    name: 'Fish',
+    component: Fish
+  },
+  {
+    path: '/bugs',
+    name: 'Bugs',
+    component: Bugs
+  },
+  {
+    path: '/fossils',
+    name: 'Fossils',
+    component: Fossils
+  },
+  {
+    path: '/art',
+    name: 'Art',
+    component: Art
+  },
+  {
+    path: '/museum',
+    name: 'Museum',
+    component: Museum,
+    children: [
+      {
+        path: 'fish',
+        name: 'MuseumFish',
+        component: MuseumFish
+      },
+      {
+        path: 'bugs',
+        name: 'MuseumBugs',
+        component: MuseumBugs
+      },
+      {
+        path: 'fossils',
+        name: 'MuseumFossils',
+        component: MuseumFossils
+      },
+      {
+        path: 'art',
+        name: 'MuseumArt',
+        component: MuseumArt
+      }
+    ]
   }
 ]
 
