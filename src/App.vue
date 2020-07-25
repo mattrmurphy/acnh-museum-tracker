d7028b83bed099f973982323cab27dfbc62857d1
Matt <mmurphydev@gmail.com>
2020-07-25 12:50:40 -0400

    adds Navigation component
    - adds the `feather-icons` package
    - removes the HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/fish">Fish</router-link>
-      <router-link to="/bugs">Bugs</router-link>
-      <router-link to="/fossils">Fossils</router-link>
-      <router-link to="/art">Art</router-link>
-      <router-link to="/museum">Museum</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <main class="h-screen mx-auto w-full md:w-4/6" id="app">
+    <Navigation />
+    <router-view class="p-4" />
+  </main>
 </template>
+
+<script>
+import Navigation from "@/components/Navigation";
+
+export default {
+  name: "App",
+  components: { Navigation },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: #333;
 }
 </style>
